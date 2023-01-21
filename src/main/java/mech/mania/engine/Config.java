8ebcfd35a1f2c5415dec689f0b712a9373d3750e
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-01-21 14:37:58 -0600

    Add config to setup field in gamelog

--- src/main/java/mech/mania/engine/Config.java
+++ src/main/java/mech/mania/engine/Config.java
@@ -1,6 +1,8 @@
 package mech.mania.engine;
 
-public class Config {
+import java.io.Serializable;
+
+public class Config implements Serializable {
     public static final int BOARD_SIZE = 100;
     public static final int TURNS = 100;
     public static final int TOTAL_CHARACTERS = 25;
--- src/main/java/mech/mania/engine/log/LogSetupState.java
+++ src/main/java/mech/mania/engine/log/LogSetupState.java
@@ -2,12 +2,22 @@ package mech.mania.engine.log;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import static mech.mania.engine.Config.*;
+
 public class LogSetupState {
-    // TODO: Add any log setup states in here
     @JsonProperty("theAnswerToLifeTheUniverseAndEverything")
     private final int theAnswerToLifeTheUniverseAndEverything;
+    @JsonProperty("boardSize")
+    private final int boardSize;
+    @JsonProperty("turns")
+    private final int turns;
+    @JsonProperty("totalCharacters")
+    private final int totalCharacters;
 
     public LogSetupState() {
         this.theAnswerToLifeTheUniverseAndEverything = 42;
+        this.boardSize = BOARD_SIZE;
+        this.turns = TURNS;
+        this.totalCharacters = TOTAL_CHARACTERS;
     }
 }
