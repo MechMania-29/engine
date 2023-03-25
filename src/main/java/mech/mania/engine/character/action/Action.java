5357771e86ffef1f0d12734fba886bca735fff77
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-03-25 15:14:25 -0500

    Remove unused valid property

--- src/main/java/mech/mania/engine/character/action/Action.java
+++ src/main/java/mech/mania/engine/character/action/Action.java
@@ -3,7 +3,6 @@ package mech.mania.engine.character.action;
 // The base action class
 public class Action {
     private String executingCharacterId; // The id of the character executing this action
-    private boolean isValid = true; // Whether this action is valid
 
     public Action(String executingCharacterId) {
         this.executingCharacterId = executingCharacterId;
@@ -11,10 +10,5 @@ public class Action {
 
     public String getExecutingCharacterId() {
         return executingCharacterId;
-    }
-
-    // Invalidates the current action, making it not valid
-    public void invalidate() {
-        isValid = false;
     }
 }
