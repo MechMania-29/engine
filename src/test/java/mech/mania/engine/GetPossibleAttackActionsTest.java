61eed29d4c72bc40ddc1f79ef0b297ca000af9ff
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-09-20 13:10:57 -0500

    Add more test cases to GetPossibleAttackActions

--- src/test/java/mech/mania/engine/GetPossibleAttackActionsTest.java
+++ src/test/java/mech/mania/engine/GetPossibleAttackActionsTest.java
@@ -23,6 +23,12 @@ public class GetPossibleAttackActionsTest {
     private final Position up1right1 = new Position(center.getX() + 1, center.getY() - 1);
     private final Position down1left1 = new Position(center.getX() - 1, center.getY() + 1);
     private final Position down1right1 = new Position(center.getX() + 1, center.getY() + 1);
+    private final Position left2 = new Position(center.getX() - 2, center.getY());
+    private final Position right2 = new Position(center.getX() + 2, center.getY());
+    private final Position up1left2 = new Position(center.getX() - 2, center.getY() - 1);
+    private final Position up1right2 = new Position(center.getX() + 2, center.getY() - 1);
+    private final Position down1left2 = new Position(center.getX() - 2, center.getY() + 1);
+    private final Position down1right2 = new Position(center.getX() + 2, center.getY() + 1);
     private final List<List<Character>> map = List.of(
             "ttttt".chars().mapToObj(ch -> (char) ch).toList(),
             "ttttt".chars().mapToObj(ch -> (char) ch).toList(),
@@ -98,5 +104,59 @@ public class GetPossibleAttackActionsTest {
         zombie.applyClassData(new CharacterClassData(1, 1, 1, 0, List.of()));
         List<AttackAction> attack = game.getPossibleAttackActions(true).get(zombie.getId());
         assertListContentsEqual(attackExpected, attack);
+    }
+
+    @Test
+    public void range2PlusHuman() {
+        List<AttackAction> attackExpected = List.of(
+                new AttackAction(human.getId(), zombie.getId(), AttackActionType.CHARACTER),
+                new AttackAction(human.getId(), up1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(human.getId(), down1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(human.getId(), up1left1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(human.getId(), down1left1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(human.getId(), up1right1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(human.getId(), down1right1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(human.getId(), left2.toString(), AttackActionType.TERRAIN),
+                new AttackAction(human.getId(), right2.toString(), AttackActionType.TERRAIN)
+        );
+        human.applyClassData(new CharacterClassData(1, 1, 2, 0, List.of()));
+        List<AttackAction> attack2 = game.getPossibleAttackActions(false).get(human.getId());
+        human.applyClassData(new CharacterClassData(1, 1, 3, 0, List.of()));
+        List<AttackAction> attack3 = game.getPossibleAttackActions(false).get(human.getId());
+        human.applyClassData(new CharacterClassData(1, 1, 4, 0, List.of()));
+        List<AttackAction> attack4 = game.getPossibleAttackActions(false).get(human.getId());
+
+        assertListContentsEqual(attackExpected, attack2);
+        assertListContentsEqual(attackExpected, attack3);
+        assertListContentsEqual(attackExpected, attack4);
+    }
+
+    @Test
+    public void range2PlusZombie() {
+        List<AttackAction> attackExpected = List.of(
+                new AttackAction(zombie.getId(), human.getId(), AttackActionType.CHARACTER),
+                new AttackAction(zombie.getId(), up1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), down1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), up1left1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), up1right1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), down1left1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), down1right1.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), left2.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), right2.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), up1left2.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), up1right2.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), down1left2.toString(), AttackActionType.TERRAIN),
+                new AttackAction(zombie.getId(), down1right2.toString(), AttackActionType.TERRAIN)
+        );
+        zombie.applyClassData(new CharacterClassData(1, 1, 2, 0, List.of()));
+        List<AttackAction> attack2 = game.getPossibleAttackActions(true).get(zombie.getId());
+        zombie.applyClassData(new CharacterClassData(1, 1, 3, 0, List.of()));
+        List<AttackAction> attack3 = game.getPossibleAttackActions(true).get(zombie.getId());
+        zombie.applyClassData(new CharacterClassData(1, 1, 4, 0, List.of()));
+        List<AttackAction> attack4 = game.getPossibleAttackActions(true).get(zombie.getId());
+
+        assertListContentsEqual(attackExpected, attack2);
+        assertListContentsEqual(attackExpected, attack3);
+        assertListContentsEqual(attackExpected, attack4);
     }
 }
