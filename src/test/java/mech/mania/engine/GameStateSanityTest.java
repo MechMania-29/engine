e506f0a5853708b3c664340f14abc2ef4890ebc0
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-09-20 13:34:03 -0500

    Add RunAGame test

--- src/test/java/mech/mania/engine/GameStateSanityTest.java
+++ src/test/java/mech/mania/engine/GameStateSanityTest.java
@@ -1,11 +1,18 @@
 package mech.mania.engine;
 
 import mech.mania.engine.character.CharacterState;
+import mech.mania.engine.log.LogErrors;
 import mech.mania.engine.player.ComputerPlayer;
+import mech.mania.engine.player.DoNothingPlayer;
 import mech.mania.engine.player.Player;
+import mech.mania.engine.terrain.MapLoader;
+import mech.mania.engine.terrain.SelectedMap;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 import static mech.mania.engine.Config.STARTING_ZOMBIES;
 import static mech.mania.engine.Config.TOTAL_CHARACTERS;
@@ -35,5 +42,21 @@ public class GameStateSanityTest {
         assertEquals(STARTING_ZOMBIES, zombiesCount);
         assertEquals(TOTAL_CHARACTERS - STARTING_ZOMBIES, humansCounts);
         assertEquals(TOTAL_CHARACTERS, totalCount);
+    }
+
+    @Test
+    public void RunAGame() throws IOException {
+        List<List<Character>> map = MapLoader.LoadMap(SelectedMap.MAIN);
+        Player human = new ComputerPlayer(false);
+        Player zombie = new ComputerPlayer(true);
+        GameState gameState = new GameState(human, zombie, map);
+
+        while (!gameState.isFinished()) {
+            gameState.runTurn();
+        }
+
+        LogErrors errors = gameState.getErrors();
+        assertEquals(0, errors.humanErrors().size(), String.format("Expected no human errors, got: %s", errors.humanErrors()));
+        assertEquals(0, errors.zombieErrors().size(), String.format("Expected no zombie errors, got: %s", errors.humanErrors()));
     }
 }
